6ad940a87fd61abeebaa9220759e897b28cda4c5
HP <rajapandian84@gmail.com>
2023-11-09 10:41:57 +0530

    First Commit

---
+++ ghtems/src/main/java/com/ght/ems/GhtemsApplication.java
@@ -0,0 +1,13 @@
+package com.ght.ems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhtemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhtemsApplication.class, args);
+	}
+
+}
---
+++ ghtems/src/main/java/com/ght/ems/controller/IndexController.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package com.ght.ems.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ght.ems.model.Address;
+
+/**
+ * 
+ */
+@Controller
+@RequestMapping({ "/", "/index" })
+public class IndexController {
+
+	@GetMapping
+	public String main(Model model) {
+		model.addAttribute("address", new Address());
+		return "index";
+	}
+
+	@PostMapping
+	public String save(Address address, Model model) {
+		model.addAttribute("address", address);
+		return "saved";
+	}
+}
